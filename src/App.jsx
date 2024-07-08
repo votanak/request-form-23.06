49ec521d9354baa7cda9e5eb7212c1aa8cf7087c
votanak <v0tanak@yandex.ru>
2024-07-08 23:44:28 +0300

    asdfa

--- src/App.jsx
+++ src/App.jsx
@@ -27,11 +27,16 @@ const AppStyle = styled.div`
 function App() {
   const [formData, setFormData] = useState({
     marketplace: '',
+    market: '',
+    performanceKey: '',
+    performanceSecret: '',
     client_id: '',
     client_key: '',
     startDate: new Date(),
     endDate: new Date(),
   });
+
+  const [showPerformance, setShowPerformance] = useState('hidden');
 
   const sendForm = (e) => {
     e.preventDefault();
@@ -57,6 +62,9 @@ function App() {
     request('/req', 'POST', {
       body: JSON.stringify({
         marketplace: formData.marketplace,
+        performanceKey: formData.performanceKey,
+        performanceSecret: formData.performanceSecret,
+        market: formData.market,
         client_id: formData.client_id,
         client_key: formData.client_key,
         startDate: formData.startDate,
@@ -65,12 +73,30 @@ function App() {
     }).then((data) => alert(data));
     setFormData({
       marketplace: '',
+      performanceKey: '',
+      performanceSecret: '',
+       market: '',
       client_id: '',
       client_key: '',
       startDate: new Date(),
       endDate: new Date(),
     });
   };
+
+
+  const marketPlaceSelectionHandle = (e) => {
+      setFormData({ ...formData, marketplace: e.target.value })
+      setShowPerformance(e.target.value === 'OZON'? '': 'hidden')
+      setFormData({
+        performanceKey: '',
+        performanceSecret: '',
+        market: '',
+        client_id: '',
+        client_key: '',
+        startDate: new Date(),
+        endDate: new Date(),
+      });
+  }
 
   console.log(formData);
 
@@ -98,13 +124,82 @@ function App() {
                   required
                   className="bg-gray-50 mx-auto sm:max-w-sm border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                   value={formData.marketplace}
-                  onChange={(e) =>
-                    setFormData({ ...formData, marketplace: e.target.value })
-                  }>
+                  onChange={marketPlaceSelectionHandle}>
                   <option value="">-- Выберите площадку --</option>
                   <option value="OZON">OZON</option>
                   <option value="WB">Wildberries</option>
                 </select>
+              </div>
+            </div>
+            
+            <div className=''>
+              <label
+                htmlFor="marketplace"
+                className="block text-sm font-medium leading-6 text-gray-900 mx-auto sm:max-w-sm">
+                Магазин
+              </label>
+              <div className="mt-2">
+                <select
+                  name="market"
+                  id="market"
+                  required
+                  className="bg-gray-50 mx-auto sm:max-w-sm border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                  value={formData.market}
+                  onChange={e => setFormData({...formData, market: e.target.value})}>
+                  <option value="">-- Выберите магазин --</option>
+                  <option value="market1">market1</option>
+                  <option value="market2">market2</option>
+                  <option value="market3">market3</option>
+                  <option value="market4">market4</option>
+                </select>
+              </div>
+            </div>
+
+            <div className={`${showPerformance} mx-auto sm:max-w-sm`}>
+              <div className="flex items-center justify-between">
+                <label
+                  htmlFor="client-id"
+                  className="block text-sm font-medium leading-6 text-gray-900 ">
+                  Performance Key
+                </label>
+              </div>
+              <div className="mt-2">
+                <input
+                  id="performance-key"
+                  name="performance-key"
+                  type="text"
+                  placeholder="Performance Key"
+                  required={formData.marketplace === 'OZON'? true :false}
+                  className="block w-full rounded-md border-0 p-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                  onChange={(e) => {
+                    setFormData({ ...formData, performanceKey: e.target.value });
+                  }}
+                  value={formData.performanceKey}
+                />
+              </div>
+            </div>
+
+            <div className={`${showPerformance} mx-auto sm:max-w-sm`}>
+              <div className="flex items-center justify-between">
+                <label
+                  htmlFor="client-id"
+                  className="block text-sm font-medium leading-6 text-gray-900 ">
+                  Performance Secret
+                </label>
+              </div>
+              <div className="mt-2">
+                <input
+                  id="performance-secret"
+                  name="performance-secret"
+                  type="text"
+                  placeholder="Performance Secret"
+                  required={formData.marketplace === 'OZON'? true :false}
+                  className="block w-full rounded-md border-0 p-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                  onChange={(e) => {
+                    setFormData({ ...formData, performanceSecret: e.target.value });
+                  }}
+                  value={formData.performanceSecret}
+                />
               </div>
             </div>
 
